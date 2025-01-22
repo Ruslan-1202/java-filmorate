f1258f58a693ebd5a58fa121acf7744551345122
Ruslan Abushaev <rus021281@gmail.com>
2025-01-22 19:07:21 +0300

    Разное

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,45 @@
 package ru.yandex.practicum.filmorate;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.UserDbStorage;
 
-@SpringBootTest
-class FilmorateApplicationTests {
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@JdbcTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Import({UserDbStorage.class})
+class FilmoRateApplicationTests {
+
+	private final UserDbStorage userStorage;
 
 	@Test
-	void contextLoads() {
-	}
+	public void testFindUserById() {
+		User user = new User();
+		user.setName("Name 1");
+		user.setEmail("email 1");
+		user.setBirthday(LocalDate.of(1990, 1, 1));
+		userStorage.create(user);
 
+		Optional<User> userOptional = userStorage.get(1L);
+
+		assertThat(userOptional)
+				.isPresent()
+				.hasValueSatisfying(a ->
+						assertThat(a).hasFieldOrPropertyWithValue("id", 1L)
+				)
+				.hasValueSatisfying(a ->
+						assertThat(a).hasFieldOrPropertyWithValue("name", "Name 1")
+				);
+	}
 }
